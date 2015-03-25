41c3331b13122277606d92ba07d017470230dae6
chahyadisgit <chahyadisgit@github.com>
2015-03-25 20:27:59 +0700

    Generate class createToken and unit testing

---
+++ JwtClient/src/main/java/com/chahyadis/jwt/ClientToken.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.chahyadis.jwt;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import com.atlassian.jwt.SigningAlgorithm;
+import com.atlassian.jwt.core.JwtUtil;
+import com.atlassian.jwt.core.TimeUtil;
+import com.atlassian.jwt.core.writer.JsonSmartJwtJsonBuilder;
+import com.atlassian.jwt.core.writer.JwtClaimsBuilder;
+import com.atlassian.jwt.core.writer.NimbusJwtWriterFactory;
+import com.atlassian.jwt.httpclient.CanonicalHttpUriRequest;
+import com.atlassian.jwt.writer.JwtJsonBuilder;
+import com.atlassian.jwt.writer.JwtWriterFactory;
+import com.chahyadis.jwt.model.UrlContextPathModel;
+
+/**
+ * ClientToken<br/>
+ * Client Authentication Token.
+ * 
+ * @author Surya Chahyadi
+ * @since March 24th, 2015
+ * @version 1.0
+ */
+public class ClientToken {
+
+	/**
+	 * createClientToken<br/>
+	 * generate token.
+	 * 
+	 * @param urlContextPathModel
+	 *            {@link UrlContextPathModel}
+	 * @param parameterMap
+	 *            {@link Map}
+	 * @return {@link String}
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
+	public String createClientToken(UrlContextPathModel urlContextPathModel,
+			Map<String, String[]> parameterMap)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		long issuedAt = TimeUtil.currentTimeSeconds();
+		long expiresAt = issuedAt + urlContextPathModel.getExpiresAt();
+		String key = urlContextPathModel.getKey();
+		String sharedSecret = JwtUtil.computeSha256Hash(urlContextPathModel
+				.getSharedSecret());
+		String method = urlContextPathModel.getMethod();
+		String contextPath = urlContextPathModel.getContextPath();
+		String apiPath = urlContextPathModel.getApiPath();
+
+		// create claim
+		JwtJsonBuilder jwtBuilder = new JsonSmartJwtJsonBuilder()
+				.issuedAt(issuedAt).expirationTime(expiresAt).issuer(key);
+
+		CanonicalHttpUriRequest canonical = new CanonicalHttpUriRequest(method,
+				apiPath, contextPath, parameterMap);
+		JwtClaimsBuilder.appendHttpRequestClaims(jwtBuilder, canonical);
+
+		// claim content
+		String jwtbuilt = jwtBuilder.build();
+
+		// create jwt token
+		JwtWriterFactory jwtWriterFactory = new NimbusJwtWriterFactory();
+		String jwtToken = jwtWriterFactory.macSigningWriter(
+				SigningAlgorithm.HS256, sharedSecret).jsonToJwt(jwtbuilt);
+
+		return jwtToken;
+	}
+
+}
---
+++ JwtClient/src/test/java/com/chahyadis/jwt/ClientTokenTest.java
@@ -0,0 +1,36 @@
+package com.chahyadis.jwt;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chahyadis.jwt.enumeration.MethodEnum;
+import com.chahyadis.jwt.model.UrlContextPathModel;
+
+import junit.framework.TestCase;
+
+public class ClientTokenTest extends TestCase {
+
+	public void testCreateClientToken() {
+		UrlContextPathModel urlContextPathModel = new UrlContextPathModel(180L,
+				"surya project", "chahyadis", MethodEnum.POST.name(),
+				"http://localhost:8080", "/jwt", "/customerServlet");
+		
+		Map<String, String[]> parameterMap = new HashMap<String, String[]>();
+		parameterMap.put("name", new String[]{"surya"});
+		parameterMap.put("gender", new String[]{"man"});
+		
+		ClientToken ct = new ClientToken();
+		String token = "";
+		try {
+			token = ct.createClientToken(urlContextPathModel, parameterMap);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(">> token: " + token);
+	}
+}
