d6ec57cb038369b03ed5d6ab75a9e0fab6893e3f
chahyadisgit <chahyadisgit@github.com>
2015-03-26 18:06:10 +0700

    Penambahan class untuk ConstantaVariable

---
+++ JwtObject/src/main/java/com/chahyadis/jwt/constant/ConstantaVariable.java
@@ -0,0 +1,12 @@
+package com.chahyadis.jwt.constant;
+
+/**
+ * Constanta.
+ * 
+ * @author Surya Chahyadi
+ * @since March 26th, 2015
+ * @version 1.0
+ */
+public interface ConstantaVariable {
+	static final String DEFAULT_SHARED_KEY = "a1dd395c19f3a1f7ff0e6bfe3ee6028e4373b41085a6da776ab02a8baf129abb";
+}
